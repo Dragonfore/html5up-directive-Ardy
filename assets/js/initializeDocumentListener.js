cbad49f2e02c3c78ef81011bca94d8d7cb2e8d74
Dragonfore <matthewmackay26@hotmail.com>
2018-01-30 11:07:29 -0800

    Updating the html
    Updating to new format of webiste and adding secondary pages.

--- assets/js/initializeDocumentListener.js
+++ assets/js/initializeDocumentListener.js
@@ -8,3 +8,42 @@ edit the class/id requirements as needed.
 
 Dependencies:
 jQuery 3.2.1*/
+
+$(document).ready(function(){
+	"use strict";
+	$(".innerA").on('click', function(event) {
+		if(this.hash !== "") {
+			event.preventDefault();
+
+			var hash = this.hash;
+
+			$('html, body').animate({
+				scrollTop: $(hash).offset().top
+			}, 800, function(){
+
+			// Add hash (#) to URL when done scrolling (default click behavior)
+			window.location.hash = hash;
+		  });
+
+
+		}
+	});
+	
+	$("#pageScroll").on('click', function(event) {
+	if(this.hash !== "") {
+		event.preventDefault();
+
+		var hash = this.hash;
+
+		$('html, body').animate({
+			scrollTop: $(hash).offset().top
+		}, 800, function(){
+
+		// Add hash (#) to URL when done scrolling (default click behavior)
+		window.location.hash = hash;
+	  });
+
+
+	}
+	});
+});
